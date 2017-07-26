f796a6dd663d553d66a478e310dd8feadb11d9c2
Jannot <denis.jannot@emc.com>
2017-07-26 08:20:39 +0100

    Add the ability to indicate the IP addresses in the command line

--- ecslogs.go
+++ ecslogs.go
@@ -12,12 +12,12 @@ var f *os.File
 
 func main() {
   if len(os.Args) != 7 && len(os.Args) != 8 {
-                log.Fatalf("Usage: %s <user> <host:port> <pattern> <input log> <# days> <pipe|file> <output file>", os.Args[0])
+                log.Fatalf("Usage: %s <user> <host:port> <pattern> <input log> <# days> <ip1,ip2,ip3|dynamic> <pipe|file> <output file>", os.Args[0])
         }
 
   var err error
-  if os.Args[6] == "file" {
-    f, err = os.Create(os.Args[7])
+  if os.Args[7] == "file" {
+    f, err = os.Create(os.Args[8])
     if err != nil {
       panic(err)
     }
@@ -25,17 +25,23 @@ func main() {
     defer f.Close()
   }
 
-        client, session, err := connectToHost(os.Args[1], os.Args[2])
-        if err != nil {
-                panic(err)
-        }
+  client, session, err := connectToHost(os.Args[1], os.Args[2])
+  if err != nil {
+          panic(err)
+  }
 
-  outIp, err := session.CombinedOutput(`sudo getrackinfo -a | egrep "MA|SA" | awk '{ print $5 }'`)
-        if err != nil {
-                panic(err)
-        }
+  ipAddresses := []string{}
+  if os.Args[6] == "dynamic" {
+    outIp, err := session.CombinedOutput(`sudo getrackinfo -a | egrep "MA|SA" | awk '{ print $5 }'`)
+    if err != nil {
+            panic(err)
+    }
+    ipAddresses = strings.Split(string(outIp),"\n")
+  } else {
+    ipAddresses = strings.Split(os.Args[6],",")
+  }
 
-  for _, ip := range strings.Split(string(outIp),"\n") {
+  for _, ip := range ipAddresses {
     if ip != "" {
       session, err := client.NewSession()
       if err != nil {
@@ -53,7 +59,7 @@ func main() {
                 }
           outGrep, _ := session.CombinedOutput(`ssh ` + ip + ` 'sudo zgrep "` + os.Args[3] + `" ` + log + `' | awk '{ print "` + ip + ` ` + log + ` "$0 }'`)
           if string(outGrep) != "" {
-            if os.Args[6] == "pipe" {
+            if os.Args[7] == "pipe" {
               fmt.Println(string(outGrep))
             } else {
               if _, err = f.Write(outGrep); err != nil {
